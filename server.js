c4c4cd5f581967a3e0b3c5c61118f7bcc41ead42
15153246027 <652992429@qq.com>
2020-07-03 13:54:22 +0800

    加密

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var https = require('https')
     ,fs = require("fs");
 
 var options = {
-    key: fs.readFileSync('./privatekey.pem'),
-    cert: fs.readFileSync('./certificate.pem')
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
 };
 
 https.createServer(options, function(req,res){
